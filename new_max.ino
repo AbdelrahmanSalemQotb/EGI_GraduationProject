ed1f2d16d7eb103e8732ce2b19444554622d61a7
Abdelrahman Salem <salem.qotb.a@gmail.com>
2022-04-15 17:27:58 +0200

    add LCD_I2C to work with code and add buzzer ,( buzzer not tested)

--- new_max.ino
+++ new_max.ino
@@ -4,8 +4,15 @@
 #include <BlynkSimpleEsp8266.h>
 #include "MAX30100_PulseOximeter.h"
 #include <Adafruit_MLX90614.h>
+#include <LiquidCrystal_I2C.h>
 
-#define REPORTING_PERIOD_MS     1000
+#define LCD_I2C_Address     0x27
+
+#define Buzzer_Pin          5
+
+LiquidCrystal_I2C lcd(LCD_I2C_Address, 20, 4);  
+
+#define REPORTING_PERIOD_MS     300
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
@@ -13,10 +20,12 @@ float object;
 int BPM,SPO2;
 
 char auth[] = "qXCkVaXrY7nfQW1Ovz4-8dy_7qpKDVjQ";             // You should get Auth Token in the Blynk App.
-char ssid[] = "mostafa";                              // Your WiFi credentials.
-char pass[] = "000011122";
+//char ssid[] = "mostafa";                              // Your WiFi credentials.
+//char pass[] = "000011122";
+char ssid[] = "Salem";                              // Your WiFi credentials.
+char pass[] = "5522333123";
 
-bool flag=0;
+byte flag=0;
 PulseOximeter pox;
 
 uint32_t tsLastReport = 0;
@@ -29,9 +38,75 @@ void onBeatDetected()
 //    SPO2 = pox.getSpO2();
 }
 
+void LCD_Setup(void)
+{
+  lcd.init();
+  
+  // Make sure backlight is on       
+  lcd.backlight();
+  
+  // create a new characters
+//  lcd.createChar(0, Heart);
+  
+  // Clears the LCD screen
+  lcd.clear();
+  
+  
+  lcd.setCursor(0, 1);
+  lcd.print("SPO2  = ");
+  
+  lcd.setCursor(0, 2);
+  lcd.print("HR    = ");
+
+  lcd.setCursor(0, 3);
+  lcd.print("TEMP  = ");
+  lcd.noBlink();
+}
+
+void LCD_Report_Data (void)
+{
+   lcd.clear();
+  
+  
+  lcd.setCursor(0, 1);
+  lcd.print("SPO2  = ");
+  
+  lcd.setCursor(0, 2);
+  lcd.print("HR    = ");
+
+  lcd.setCursor(0, 3);
+  lcd.print("TEMP  = ");
+  
+  lcd.setCursor(8, 1);
+  lcd.print("       ");
+  lcd.setCursor(8, 1);
+  lcd.print(SPO2);
+  lcd.setCursor(12, 1);
+  lcd.print('%');
+  
+  lcd.setCursor(8, 2);
+  lcd.print("       ");
+  lcd.setCursor(8, 2);
+  lcd.print(BPM);
+  lcd.setCursor(12, 2);
+  lcd.print("BPM");
+
+
+  lcd.setCursor(8, 3);
+  lcd.print("       ");
+  lcd.setCursor(8, 3);
+  lcd.print((int)object);
+  lcd.setCursor(12, 3);
+  lcd.print((char)0xDF);
+  lcd.print("C");
+
+}
+
 void setup()
 {
     Blynk.begin(auth, ssid, pass);
+
+    LCD_Setup();   
     Serial.begin(115200);
     if (!mlx.begin(0x5A)) 
     {
@@ -39,17 +114,18 @@ void setup()
     }
     Wire.setClock(100000UL);
     Serial.print("Initializing pulse oximeter..");
-    
+
+    // init buzzer
+    pinMode(Buzzer_Pin,OUTPUT);
     // Initialize the PulseOximeter instance
     // Failures are generally due to an improper I2C wiring, missing power supply
     // or wrong target chip
     if (!pox.begin()) {
         Serial.println("FAILED");
-        for(;;);
     } else {
         Serial.println("SUCCESS");
     }
-    pox.setOnBeatDetectedCallback(onBeatDetected);
+//    pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop()
@@ -71,20 +147,50 @@ void loop()
             SPO2 = pox.getSpO2();
             Serial.print(SPO2);
             Serial.println("%");
+            if (SPO2>100&& SPO2<0)
+            {
+              SPO2=0;
+            }
             
+            flag=1;
+          }
+        else if ( flag ==1)
+        {
             object=mlx.readObjectTempC(); 
             (object>0)&&(object<100)? : object=0;
             Serial.print("Object = "); Serial.print(object);
             Serial.println();
-            flag=1;
-          }
+            flag=2;
+        }
+        else if (flag ==2)
+        {
+          LCD_Report_Data();
+          flag=3;
+
+        }
+        else if ( flag == 3 )
+        {
+              if ((SPO2 > 0 && SPO2 < 90) || ( BPM > 100) || (object > 37))
+              {
+                digitalWrite(Buzzer_Pin,HIGH);
+              }
+              else
+              {
+                digitalWrite(Buzzer_Pin,LOW);
+              }
+              flag=4;
+        
+        }
         else
         {
             Blynk.virtualWrite(V3, BPM);
             Blynk.virtualWrite(V4, SPO2);
             Blynk.virtualWrite(V5, object);
+            
             flag=0;
         }
-        tsLastReport = millis();
+
+          tsLastReport = millis();
+
     }
 }
