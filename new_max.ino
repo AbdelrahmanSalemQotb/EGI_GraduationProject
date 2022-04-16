cc67b3ddcd5eaeb71f99f834db7bad93e08e1894
Abdelrahman Salem <salem.qotb.a@gmail.com>
2022-04-16 13:11:52 +0200

    fix buzzer + update to Blynk V2.0

--- new_max.ino
+++ new_max.ino
@@ -1,3 +1,7 @@
+#define BLYNK_TEMPLATE_ID "TMPLak1A52NB"
+#define BLYNK_DEVICE_NAME "EGI"
+#define BLYNK_AUTH_TOKEN "xNSASj75s6HnPpghBgUfpXH8MkvoLlXj"
+#define Serial_Debug
 #include <Wire.h>
 #include <Blynk.h>
 #include <ESP8266WiFi.h>
@@ -6,36 +10,40 @@
 #include <Adafruit_MLX90614.h>
 #include <LiquidCrystal_I2C.h>
 
-#define LCD_I2C_Address     0x27
+#define REPORTING_PERIOD_MS     300
 
-#define Buzzer_Pin          5
+#define LCD_I2C_Address     0x27
 
 LiquidCrystal_I2C lcd(LCD_I2C_Address, 20, 4);  
 
-#define REPORTING_PERIOD_MS     300
+#define Buzzer_Pin          D5
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-
 float object;
-int BPM,SPO2;
 
-char auth[] = "qXCkVaXrY7nfQW1Ovz4-8dy_7qpKDVjQ";             // You should get Auth Token in the Blynk App.
+
+
+//char auth[] = "qXCkVaXrY7nfQW1Ovz4-8dy_7qpKDVjQ";             // You should get Auth Token in the Blynk App.
 //char ssid[] = "mostafa";                              // Your WiFi credentials.
 //char pass[] = "000011122";
 char ssid[] = "Salem";                              // Your WiFi credentials.
 char pass[] = "5522333123";
 
 byte flag=0;
+
 PulseOximeter pox;
+int BPM,SPO2;
 
 uint32_t tsLastReport = 0;
 
 // Callback (registered below) fired when a pulse is detected
 void onBeatDetected()
 {
+  #ifdef Serial_Debug
     Serial.println("Beat!");
-//    BPM = pox.getHeartRate();
-//    SPO2 = pox.getSpO2();
+   #endif
+    BPM = pox.getHeartRate();
+    SPO2 = pox.getSpO2();
 }
 
 void LCD_Setup(void)
@@ -104,28 +112,42 @@ void LCD_Report_Data (void)
 
 void setup()
 {
-    Blynk.begin(auth, ssid, pass);
+    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 
     LCD_Setup();   
+    
+    #ifdef Serial_Debug
     Serial.begin(115200);
+    #endif
+    
     if (!mlx.begin(0x5A)) 
     {
+       #ifdef Serial_Debug
       Serial.println("temp");
+      #endif
     }
     Wire.setClock(100000UL);
+    #ifdef Serial_Debug
     Serial.print("Initializing pulse oximeter..");
-
+    #endif
     // init buzzer
     pinMode(Buzzer_Pin,OUTPUT);
+
+    
     // Initialize the PulseOximeter instance
     // Failures are generally due to an improper I2C wiring, missing power supply
     // or wrong target chip
     if (!pox.begin()) {
-        Serial.println("FAILED");
+        #ifdef Serial_Debug
+          Serial.println("FAILED");
+        #endif
+          
     } else {
-        Serial.println("SUCCESS");
+        #ifdef Serial_Debug
+          Serial.println("SUCCESS");
+        #endif
     }
-//    pox.setOnBeatDetectedCallback(onBeatDetected);
+    pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop()
@@ -140,13 +162,16 @@ void loop()
     if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
         if(flag==0)
           {
-            Serial.print("Heart rate:");
             BPM = pox.getHeartRate();
+            SPO2 = pox.getSpO2();
+            
+            #ifdef Serial_Debug
+            Serial.print("Heart rate:");
             Serial.print(BPM);
             Serial.print("bpm / SpO2:");
-            SPO2 = pox.getSpO2();
             Serial.print(SPO2);
             Serial.println("%");
+            #endif
             if (SPO2>100&& SPO2<0)
             {
               SPO2=0;
@@ -156,10 +181,18 @@ void loop()
           }
         else if ( flag ==1)
         {
-            object=mlx.readObjectTempC(); 
-            (object>0)&&(object<100)? : object=0;
-            Serial.print("Object = "); Serial.print(object);
-            Serial.println();
+              if ((SPO2 > 0 && SPO2 < 90) || ( BPM > 100) || (object > 37))
+              {
+                digitalWrite(Buzzer_Pin,HIGH);
+                Serial.println(" Buzzer ON");
+              }
+              else
+              {
+                digitalWrite(Buzzer_Pin,LOW);
+                
+                Serial.println(" Buzzer OFF");
+
+              }
             flag=2;
         }
         else if (flag ==2)
@@ -170,16 +203,14 @@ void loop()
         }
         else if ( flag == 3 )
         {
-              if ((SPO2 > 0 && SPO2 < 90) || ( BPM > 100) || (object > 37))
-              {
-                digitalWrite(Buzzer_Pin,HIGH);
-              }
-              else
-              {
-                digitalWrite(Buzzer_Pin,LOW);
-              }
+            object=mlx.readObjectTempC(); 
+            (object>0)&&(object<100)? : object=0;
+            #ifdef Serial_Debug
+            Serial.print("Object = "); Serial.print(object);
+            Serial.println();
+            #endif
+
               flag=4;
-        
         }
         else
         {
