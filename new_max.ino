19352a28fc6cbd3a14fc7550e73afaafee2c1ec5
Abdelrahman Salem <salem.qotb.a@gmail.com>
2022-04-20 17:49:17 +0200

    add ir sensor .. solve the max30100 problem .. using (switch) insted of (if) {need to add server connect after disconnect}

--- new_max.ino
+++ new_max.ino
@@ -14,185 +14,149 @@
 #include "MAX30100_PulseOximeter.h"
 #include <Adafruit_MLX90614.h>
 #include <LiquidCrystal_I2C.h>
-#include <WiFiManager.h> 
+#include <WiFiManager.h>
 
-#define MANAGER_USER            "EGI"
-#define MANAGER_PASS            "EGI1718169"
-#define REPORTING_PERIOD_MS     350
-#define LCD_I2C_Address         0x27
-#define Buzzer_Pin              D5
+#define MANAGER_USER "EGI"
+#define MANAGER_PASS "EGI1718169"
+#define REPORTING_PERIOD_MS 350
+#define LCD_I2C_Address 0x27
+#define Buzzer_Pin D5
+#define IR_Pin D6
 
-
-LiquidCrystal_I2C lcd(LCD_I2C_Address, 20, 4);  
+LiquidCrystal_I2C lcd(LCD_I2C_Address, 20, 4);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 PulseOximeter pox;
 
-int   BPM   ;
-int   SPO2  ;
+int BPM;
+int SPO2;
 byte flag = 0;
-float object ;
+float object;
 int ambiant;
 uint32_t tsLastReport = 0;
 
 byte Heart[8] = {
-  0b00000,
-  0b01010,
-  0b11111,
-  0b11111,
-  0b01110,
-  0b00100,
-  0b00000,
-  0b00000
-};
+    0b00000,
+    0b01010,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,
+    0b00000,
+    0b00000};
 
-String ssid = "Salem";                              // Your WiFi credentials.
-String pass = "5522333123";
-
-
+String ssid = ""; // Your WiFi credentials.
+String pass = "";
 
 // Callback (registered below) fired when a pulse is detected
 void onBeatDetected()
 {
-  #ifdef Serial_Debug
-    Serial.println("Beat!");
-   #endif
-    BPM = pox.getHeartRate();
-    SPO2 = pox.getSpO2();
-    if (SPO2>100 || SPO2<0)
-            {
-              SPO2=0;
-            }
+#ifdef Serial_Debug
+  Serial.println("Beat!");
+#endif
+  BPM = pox.getHeartRate();
+  SPO2 = pox.getSpO2();
+  if (SPO2 > 100 || SPO2 < 0)
+  {
+    SPO2 = 0;
+  }
+  if (!digitalRead(IR_Pin))
+  {
+    SPO2 = 0;
+    BPM = 0;
+  }
 }
 
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-void wifiSetup (void)
+// WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+void wifiSetup(void)
 {
-   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // it is a good practice to make sure your code sets wifi mode how you want it.
 
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    
-    WiFiManager wm;
-    
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
-    // reset settings - wipe stored credentials for testing
-    // these are stored by the esp library
-//    wm.resetSettings();
+  WiFiManager wm;
 
-    // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
+  // reset settings - wipe stored credentials for testing
+  // these are stored by the esp library
+  //    wm.resetSettings();
 
-    bool res;
+  // Automatically connect using saved credentials,
+  // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+  // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+  // then goes into a blocking loop awaiting configuration and will return success result
+
+  bool res;
+  lcd.setCursor(0, 0);
+  lcd.print("Connect To Manager");
+  lcd.setCursor(0, 1);
+  lcd.print("User > ");
+  lcd.print(MANAGER_USER);
+  lcd.setCursor(0, 2);
+  lcd.print("Pass > ");
+  lcd.print(MANAGER_PASS);
+  lcd.setCursor(0, 3);
+  lcd.print("IP > ");
+  lcd.print("192.168.4.1");
+
+  res = wm.autoConnect(MANAGER_USER, MANAGER_PASS); // password protected ap
+
+  if (!res)
+  {
+#ifdef Serial_Debug
+    Serial.println("Failed to connect");
+#endif
+    lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("connect to manager");
+    lcd.print("  Failed to connect");
     lcd.setCursor(0, 1);
-    lcd.print("User > ");
-    lcd.print(MANAGER_USER);
-    lcd.setCursor(0, 2);
-    lcd.print("Pass > ");
-    lcd.print(MANAGER_PASS);
-    lcd.setCursor(0, 3);
-    lcd.print("ip > ");
-    lcd.print("192.168.4.1");
-
-    res = wm.autoConnect(MANAGER_USER,MANAGER_PASS); // password protected ap
-    
-    if(!res) {
-        #ifdef Serial_Debug
-        Serial.println("Failed to connect");
-        #endif
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("  Failed to connect");
-        lcd.setCursor(0, 1);
-        lcd.print("");
-        
-          
-    } 
-    else {
-        ssid = wm.getWiFiSSID();
-        pass = wm.getWiFiPass();
-        //if you get here you have connected to the WiFi   
-        #ifdef Serial_Debug 
-        Serial.println("connected... :)");
-        Serial.println(ssid);
-        Serial.println(pass);
-        #endif
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("WIFI Connected");
-    }
+    lcd.print("");
+  }
+  else
+  {
+    ssid = wm.getWiFiSSID();
+    pass = wm.getWiFiPass();
+// if you get here you have connected to the WiFi
+#ifdef Serial_Debug
+    Serial.println("Connected... :)");
+    Serial.println(ssid);
+    Serial.println(pass);
+#endif
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("WIFI Connected");
+  }
 }
 
 void LCD_Setup(void)
 {
   lcd.init();
-  
-  // Make sure backlight is on       
+
+  // Make sure backlight is on
   lcd.backlight();
-  lcd.createChar(0,Heart);
+  lcd.createChar(0, Heart);
   // Clears the LCD screen
-  lcd.clear(); 
+  lcd.clear();
 }
 
-void LCD_Report_Data (void)
+void LCD_Report_Data(void)
 {
-  //////////////////////////////////////////////////////
-//  if(WiFi.status() == WL_CONNECTED)
-//    {
-//      #ifdef Serial_Debug
-//      Serial.println("WIFI Connected");
-//      #endif
-//      lcd.setCursor(0, 0);
-//      lcd.print("WIFI  ");
-//      lcd.write(1);
-//    }
-//    else
-//    {
-//      #ifdef Serial_Debug
-//      Serial.println("WIFI Not Connected");
-//      #endif
-//      lcd.setCursor(0, 0);
-//      lcd.print("WIFI  ");
-//      lcd.write(2);
-//    }
-//    
-//  if(!Blynk.connect())
-//    {
-//      #ifdef Serial_Debug
-//      Serial.println("Server Not Connected");
-//      #endif
-//      lcd.print("  Server  ");
-//      lcd.write(2);
-//    }
-//    else
-//    {
-//      #ifdef Serial_Debug
-//      Serial.println("Server Connected");
-//      #endif
-//      lcd.print("  Server  ");
-//      lcd.write(1);
-//    }
-//  ////////////////////////////////////////////////
   lcd.setCursor(0, 1);
   lcd.print("SPO2  = ");
-  
+
   lcd.setCursor(0, 2);
   lcd.print("HR    = ");
 
   lcd.setCursor(0, 3);
   lcd.print("TEMP  = ");
-  
+
   lcd.setCursor(8, 1);
   lcd.print("       ");
   lcd.setCursor(8, 1);
   lcd.print(SPO2);
   lcd.setCursor(12, 1);
   lcd.print('%');
-  
+
   lcd.setCursor(8, 2);
   lcd.print("       ");
   lcd.setCursor(8, 2);
@@ -201,7 +165,6 @@ void LCD_Report_Data (void)
   lcd.write(0);
   lcd.print("BPM");
 
-  
   lcd.setCursor(8, 3);
   lcd.print("       ");
   lcd.setCursor(8, 3);
@@ -209,163 +172,165 @@ void LCD_Report_Data (void)
   lcd.setCursor(12, 3);
   lcd.print((char)0xDF);
   lcd.print("C");
-
 }
 
 void setup()
 {
-    #ifdef Serial_Debug
-    Serial.begin(115200);
-    #endif
-    
-    LCD_Setup();   
-    wifiSetup();
-    
-//    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-if(WiFi.status() == WL_CONNECTED){
+#ifdef Serial_Debug
+  Serial.begin(115200);
+#endif
+  LCD_Setup();
+  wifiSetup();
+
+  //    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  if (WiFi.status() == WL_CONNECTED)
+  {
     Blynk.config(BLYNK_AUTH_TOKEN);
-    if(!Blynk.connect())
+    if (!Blynk.connect())
     {
-      #ifdef Serial_Debug
+#ifdef Serial_Debug
       Serial.println("Server Not Connected");
-      #endif
+#endif
       lcd.setCursor(0, 1);
       lcd.print("Server Not Connected");
-      delay (1000);
+      delay(1000);
     }
     else
     {
-      #ifdef Serial_Debug
+#ifdef Serial_Debug
       Serial.println("Server Connected");
-      #endif
+#endif
       lcd.setCursor(0, 0);
       lcd.print("Server Connected");
-      delay (1000);
+      delay(1000);
     }
-}
-    
+  }
 
-    
-    if (!mlx.begin(0x5A)) 
-    {
-       #ifdef Serial_Debug
-      Serial.println("mlx90614 not found");
-      #endif
-    }
-    Wire.setClock(100000UL);
-    #ifdef Serial_Debug
-    Serial.print("Initializing pulse oximeter..");
-    #endif
-    // init buzzer
-    pinMode(Buzzer_Pin,OUTPUT);
+  if (!mlx.begin(0x5A))
+  {
+#ifdef Serial_Debug
+    Serial.println("MLX90614 NOT FOUND");
+#endif
+  }
+  Wire.setClock(100000UL);
+#ifdef Serial_Debug
+  Serial.print("Initializing pulse oximeter..");
+#endif
+  // init buzzer
+  pinMode(Buzzer_Pin, OUTPUT);
+  pinMode(IR_Pin, INPUT);
 
-    
-    // Initialize the PulseOximeter instance
-    // Failures are generally due to an improper I2C wiring, missing power supply
-    // or wrong target chip
-    if (!pox.begin()) {
-        #ifdef Serial_Debug
-          Serial.println("FAILED");
-        #endif
-          
-    } else {
-        #ifdef Serial_Debug
-          Serial.println("SUCCESS");
-        #endif
-    }
-    pox.setOnBeatDetectedCallback(onBeatDetected);
+  // Initialize the PulseOximeter instance
+  if (!pox.begin())
+  {
+#ifdef Serial_Debug
+    Serial.println("FAILED");
+#endif
+  }
+  else
+  {
+#ifdef Serial_Debug
+    Serial.println("SUCCESS");
+#endif
+  }
+  pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop()
 {
-  if(WiFi.status() == WL_CONNECTED)
-  // if (Blynk.connected())
+  if (WiFi.status() == WL_CONNECTED)
   {
     Blynk.run();
-  } 
-    // Make sure to call update as fast as possible
-    pox.update();
+  }
+  // Make sure to call update as fast as possible
+  pox.update();
 
-    // Asynchronously dump heart rate and oxidation levels to the serial
-    // For both, a value of 0 means "invalid"
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-        if(flag==0)
-          {
-            BPM = pox.getHeartRate();
-            SPO2 = pox.getSpO2();
-            
-            #ifdef Serial_Debug
-            Serial.print("Heart rate:");
-            Serial.print(BPM);
-            Serial.print("bpm / SpO2:");
-            Serial.print(SPO2);
-            Serial.println("%");
-            #endif
-            if (SPO2>100 || SPO2<0)
-            {
-              SPO2=0;
-            }
-            
-            flag=1;
-          }
-        else if ( flag ==1)
-        {
-              if ((SPO2 > 0 && SPO2 < 90) || ( BPM > 100) || (object > 37))
-              {
-                digitalWrite(Buzzer_Pin,HIGH);
-                #ifdef Serial_Debug
-                Serial.println(" Buzzer ON");
-                #endif
-              }
-              else
-              {
-                digitalWrite(Buzzer_Pin,LOW);
-                #ifdef Serial_Debug
-                Serial.println(" Buzzer OFF");
-                #endif
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS)
+  {
+    switch (flag)
+    {
+    case 0:
+      BPM = pox.getHeartRate();
+      SPO2 = pox.getSpO2();
 
-              }
-            flag=2;
-        }
-        else if (flag == 2)
-        {
-          LCD_Report_Data();
-          flag=3;
+      #ifdef Serial_Debug
+        Serial.print("Heart rate:");
+        Serial.print(BPM);
+        Serial.print("bpm / SpO2:");
+        Serial.print(SPO2);
+        Serial.println("%");
+      #endif
+      if (SPO2 > 100 || SPO2 < 0)
+      {
+        SPO2 = 0;
+      }
 
-        }
-        else if ( flag == 3 )
-        {
-            object = mlx.readObjectTempC();
-//            ambiant = mlx.readAmbientTempC();
-            #ifdef Serial_Debug
-            Serial.print("Object = "); Serial.print(object);
-            Serial.println();
-//            Serial.print("ambiant = "); Serial.print(ambiant);
-//            Serial.println();
-            #endif
+      flag = 1;
+      break;
 
-              flag=4;
-        }
-        else if (Blynk.connected() && flag == 4 )
-        {
-            Blynk.virtualWrite(V3, BPM);
-            Blynk.virtualWrite(V4, SPO2);
-            Blynk.virtualWrite(V5, object);
-            
-            flag=0;
-        }
-        else
-        {
-          flag=0;
-          #ifdef Serial_Debug
-          Serial.println("Server disconnected");
-          #endif
-          lcd.setCursor(0, 0);
-          lcd.print("Server disconnected");
+    case 1:
+      if ((SPO2 > 0 && SPO2 < 90) || (BPM > 100) || (object > 37))
+      {
+        digitalWrite(Buzzer_Pin, HIGH);
+        #ifdef Serial_Debug
+          Serial.println(" Buzzer ON");
+        #endif
+      }
+      else
+      {
+        digitalWrite(Buzzer_Pin, LOW);
+        #ifdef Serial_Debug
+          Serial.println(" Buzzer OFF");
+        #endif
+      }
+      flag = 2;
+      break;
 
-        }
+    case 2:
+      if (digitalRead(IR_Pin))
+      {
+        object = mlx.readObjectTempC();
+      }
+      else
+      {
+        object = 0;
+        SPO2 = 0;
+        BPM = 0;
+      }
 
-          tsLastReport = millis();
+      #ifdef Serial_Debug
+        Serial.print("Object = ");
+        Serial.print(object);
+        Serial.println();
+      #endif
+      flag = 3;
+      break;
 
+    case 3:
+      LCD_Report_Data();
+      flag = 4;
+      break;
+
+    case 4:
+      if (Blynk.connected())
+      {
+        Blynk.virtualWrite(V3, BPM);
+        Blynk.virtualWrite(V4, SPO2);
+        Blynk.virtualWrite(V5, object);
+        flag = 0;
+      }
+      break;
+
+    default:
+      // server not connected ( hence no report )
+      flag = 0;
+      #ifdef Serial_Debug
+        Serial.println("Server disconnected");
+      #endif
+      lcd.setCursor(0, 0);
+      lcd.print("Server disconnected");
+      break;
     }
+    tsLastReport = millis();
+  }
 }
